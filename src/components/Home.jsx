5d92374c7b46b75e33e88314e0d3d361f90e63ea
Ankit Tyagi <ancorp007@gmail.com>
2023-07-07 23:34:12 +0530

    change home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
               <div className="row">
                 <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1  d-flex justify-content-center flex-column">
                   <h1>
-                    Hi, my name is{" "}
+                    Hi all, I'm
                     <strong className="brand-name">Ankit Tyagi</strong>
                   </h1>
                   <h2 className="my-3">
